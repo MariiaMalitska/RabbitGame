d6431c872a93237efbb7a8d6d3eb8c688758fd85
MariiaMalitska <malitska.m@gmail.com>
2018-05-27 20:48:46 +0300

    First lab

---
+++ Assets/Content/Rabit/HeroFollow.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroFollow : MonoBehaviour {
+
+	public HeroController rabbit;
+	
+	void Update () {
+		
+	}
+}
