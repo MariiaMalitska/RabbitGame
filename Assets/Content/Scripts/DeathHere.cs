61ef2a2fbb4caa77ff8932efda359e696e2a7ed4
MariiaMalitska <malitska.m@gmail.com>
2018-06-04 15:49:39 +0300

    Lab 02

---
+++ Assets/Content/Scripts/DeathHere.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathHere : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		HeroController rabbit = collider.GetComponent<HeroController>();
+		if (rabbit!=null)
+			LevelController.current.onRabbitDeath(rabbit);
+	}
+}
